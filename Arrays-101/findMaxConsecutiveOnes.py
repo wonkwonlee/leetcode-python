4513efd805e8bfcb5aa916a09230512bbf90648f
Ray Wonkwon Lee <dload1392@gmail.com>
2021-03-20 21:14:19 +0900

    Update findMaxConsecutiveOnes.py
    Update findMaxConsecutiveOnes.py.
    Now works fine.

--- Arrays-101/findMaxConsecutiveOnes.py
+++ Arrays-101/findMaxConsecutiveOnes.py
@@ -1,22 +1,22 @@
+'''
+파이썬에서는 숫자는 정수, 실수 상관없이 0이면 거짓, 0이 아닌 수는 참이다.
+따라서 if (i == 1) : ~~ 대신, if i : ~~~ 이렇게 적어도 된다.
+
+'''
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        # Maximum consecutive value
-        result = 0
-        # Hold current maximum number of consecutive
-        count = 0        
+        # Store max value and count
+        result , count = 0 , 0       
         
-        for i in range(1, len(nums)):
-            if nums[i] == 1:
+        for i in nums:
+            # 만약 nums가 1,0 말고 다른 숫자가 있다면 i == 1,2 등 구체적으로 적어야함
+            if i :
                 count += 1
-                result = max(count, result)
-                for j in range(1, len(nums)-i):
-                    if nums[i+j] == 1:
-                        count += 1
-                        result = max(count, result)
-                    else:
-                        count = 0       # Reset
-                        break
-            else:
+            else :
                 count = 0
+            # Compare count and max value
+            result = max(count, result)
             
         return result
+
+    
