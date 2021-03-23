5978dbeadc505999944bb9744d9acb8e44f034a4
Ray Wonkwon Lee <dload1392@gmail.com>
2021-03-23 17:04:35 +0900

    Create removeElement.py
    Solve Arrays 101 Remove element problem.

---
+++ Arrays-101/removeElement.py
@@ -0,0 +1,21 @@
+'''
+Remove Element
+Given an array nums and a value val, remove all instances of that value 
+in-place and return the new length.
+'''
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if nums.count(val) < 0:
+            return 0
+        
+        for i in range(nums.count(val)):
+            nums.remove(val)
+        
+        length = len(nums)
+        
+        return length        
