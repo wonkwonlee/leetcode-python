6df29274634cab0cddd086eac6ed9d1d3a701887
wonkwonlee <dload1392@gmail.com>
2021-03-25 16:55:05 +0900

    Update validMountainArray.py
    Fix code.

--- Arrays-101/validMountainArray.py
+++ Arrays-101/validMountainArray.py
@@ -6,28 +6,29 @@ valid mountain array.
 
 Result : 188 ms
 """
+
+
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         mx = max(arr)
 
         # If max value is the first or the last element
-        if mx = arr[0] or mx = arr[len(arr)-1]:
+        if arr[0] == mx or arr[len(arr) - 1] == mx:
             return False
 
         mx_idx = arr.index(mx)
 
         # Strictly increasing and strictly decreasing arrays
-        inc, dec = arr[0:mx_idx], arr[mx_idx+1:0]
+        inc, dec = arr[0:mx_idx + 1], arr[mx_idx + 1:]
 
         # Check whether the array is strictly increasing
-        for i in range(len(inc)):
-            if inc[i] <= inc[i-1]:
+        for i in range(1, len(inc)):
+            if inc[i] <= inc[i - 1]:
                 return False
 
         # Check whether the array is strictly decreasing
-        for i in range(len(dec)):
-            if dec[i] >= dec[i-1]:
+        for i in range(1, len(dec)):
+            if dec[i] >= dec[i - 1]:
                 return False
-        
-        return True
 
+        return True
