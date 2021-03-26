0678f01affc6e18269fe28e750a56151eba16272
wonkwonlee <dload1392@gmail.com>
2021-03-26 14:24:29 +0900

    Update removeDuplicates.py
    Code update.
    Better time complexity.

--- Arrays-101/removeDuplicates.py
+++ Arrays-101/removeDuplicates.py
@@ -3,7 +3,7 @@ Remove Duplicates from Sorted Array
 Given a sorted array nums, remove the duplicates in-place such that 
 each element appears only once and returns the new length.
 
-Result : 80 ms
+Result : 72 ms
 """
 
 
@@ -17,15 +17,20 @@ class Solution:
         if len(nums) == 0:
             return 0
 
-        # Max value = last element in list. (Ascending order)
+        # Max value = last element in the list. (Ascending order)
         mx = nums[len(nums) - 1]
-        # Out of min boundary(-10000)    
+        # Less than minimum boundary 
         mn = -10001
 
-        i = 0  # index
+        count = 0  # index
         for x in nums:
             if x > mn:
-                nums[i] = x  # Update with new element
-                i += 1
+                nums[count] = x  # Update with new element
+                count += 1
                 mn = x  # Update current min value
-        return i
+
+            # Break the loop if x reaches max value
+            if x == mx:
+                break
+
+        return count
