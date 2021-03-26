38215957e7c42f1aa43073431413b0c490397e44
wonkwonlee <dload1392@gmail.com>
2021-03-26 14:25:00 +0900

    Create moveZeroes.py
    Create LeetCode Arrays 101 problem moveZeroes.py.

---
+++ Arrays-101/moveZeroes.py
@@ -0,0 +1,16 @@
+"""
+Move Zeroes
+Given an integer array nums, move all 0's to the end of it while maintaining 
+the relative order of the non-zero elements.
+
+Result : 
+"""
+
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
