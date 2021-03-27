19474a29f3e2d9abc2a206d6eb7f16fc4376ed30
wonkwonlee <dload1392@gmail.com>
2021-03-27 20:02:06 +0900

    Update sortArrayByParity.py
    Update sortArrayByParity.py.

--- Arrays-101/sortArrayByParity.py
+++ Arrays-101/sortArrayByParity.py
@@ -11,4 +11,16 @@ You may return any answer array that satisfies this condition.
 
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        
+
+        val = 0
+        length = len(A)
+        if length % 2 == 1:
+            length += 1
+
+        for i in range(length/2):
+            if A[i] % 2 == 1:
+                val = A[i]
+            if A[i] % 2 == 0:
+                A[even] = A[i]
+                
+
