ff34c97e7518b1f8ebf7866312b16885e14df416
wonkwonlee <dload1392@gmail.com>
2021-03-27 19:25:18 +0900

    Create sortArrayByParity.py
    Create LeetCode Arrays 101 problem, sortArrayByParity.py.

---
+++ Arrays-101/sortArrayByParity.py
@@ -0,0 +1,14 @@
+"""
+Sort Array By Parity
+
+Given an array A of non-negative integers, return an array consisting of 
+all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+"""
+
+
+
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        
