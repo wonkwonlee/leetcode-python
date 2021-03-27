d1c34e3e62131799cdcfd48be6eee0ed27634ebf
wonkwonlee <dload1392@gmail.com>
2021-03-27 22:33:17 +0900

    Create removeElement_2.py
    Solve removeElement in LeetCode Arrays 101.
    Use pythonic and simpler code than previous removeElement.py

---
+++ Arrays-101/removeElement_2.py
@@ -0,0 +1,25 @@
+"""
+Remove Element
+Given an array nums and a value val, remove all instances of that value 
+in-place and return the new length.
+
+The order of elements can be changed. 
+
+Simplified and 'Pythonic' version of removeElement.py
+
+Result: 24 ms
+"""
+
+
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if nums.count(val) < 0:
+            return 0
+
+        [nums.remove(val) for x in range(nums.count(val))]
+        return len(nums)
