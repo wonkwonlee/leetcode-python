3f62ed350bef15b58caf6f4bd6f84943bbc2b27a
wonkwonlee <dload1392@gmail.com>
2021-03-27 22:20:24 +0900

    Solve sortArrayByParity.py in Arrays 101 problem.

--- Arrays-101/sortArrayByParity.py
+++ Arrays-101/sortArrayByParity.py
@@ -5,22 +5,33 @@ Given an array A of non-negative integers, return an array consisting of
 all the even elements of A, followed by all the odd elements of A.
 
 You may return any answer array that satisfies this condition.
-"""
 
+Result: 72 ms
+"""
 
 
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
 
-        val = 0
-        length = len(A)
-        if length % 2 == 1:
-            length += 1
+        count = 0
 
-        for i in range(length/2):
-            if A[i] % 2 == 1:
-                val = A[i]
+        for i in range(len(A)):
             if A[i] % 2 == 0:
-                A[even] = A[i]
-                
+                # Change current val with count val
+                A[i], A[count] = A[count], A[i]
+                count += 1
 
+        return A
+
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        start = 0
+        end = len(A) - 1
+
+        # Use two pointers to go through both ends.
+        while start < end:
+            if A[start] % 2 == 0:
+                start += 1
+            elif A[end] % 2 != 0:
+                end -= 1
+            A[start], A[end] = A[end], A[start]
+        return A
