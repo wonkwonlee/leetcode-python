676eff057ad5214ab0ebe66619e60f4af653f11d
wonkwonlee <dload1392@gmail.com>
2021-03-31 22:16:20 +0900

    Create 0005_longestPalindrome.py
    Create longestPalindrome, LeetCode problem 5.

---
+++ Problems/0005_longestPalindrome.py
@@ -0,0 +1,11 @@
+"""
+Longest Palindromic Substring
+Given a string s, return the longest palindromic substring in s.
+
+Palindrome is a word which reads the same backward as forward, such as madam.
+
+Result: 
+"""
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
