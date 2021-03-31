ef8fc9454c1ea27963783059b36ab73e82845a2f
wonkwonlee <dload1392@gmail.com>
2021-03-31 22:17:02 +0900

    Update hasCycle.py
    Create hasCycle in LeetCode Linked List.
    Need modification.

--- Linked-List/hasCycle.py
+++ Linked-List/hasCycle.py
@@ -1,12 +1,13 @@
 """
 Linked List Cycle
 Given head, the head of a linked list, determine if the linked list has a cycle in it.
-Return true if there is a cycle in the linked list. Otherwise, return false.
+Return True if there is a cycle in the linked list. Otherwise, return False.
 
 A cycle in a linked list is defined if there is some node in the list that can be 
 reached again by continuously following the next pointer. 
 Internally, pos is used to denote the index of the node that tail's next pointer 
-is connected to. Note that pos is not passed as a parameter.
+is connected to. 
+Note that pos is not passed as a parameter.
 
 
 
@@ -19,5 +20,25 @@ Result:
 #         self.next = None
 
 class Solution:
+    def isCycle(val):
+        set_val = list(set(val))
+        if len(set_val) == len(val):
+            return True
+        else:
+            return False
+
+
     def hasCycle(self, head: ListNode) -> bool:
         
+        if head.next is None:
+            return False
+
+        curr = head
+        curr_val = []
+        while True:
+            curr_val.append(curr.val)
+            if curr.next is None:
+                return False
+            else:
+                curr = head.next
+
