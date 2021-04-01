7991549d27c3c9e8ccf33e54e84c603c8116d87a
wonkwonlee <dload1392@gmail.com>
2021-04-01 22:21:50 +0900

    Finish hasCycle.py
    Solve hasCycle.py LeetCode Linked List problem.

--- Linked-List/hasCycle.py
+++ Linked-List/hasCycle.py
@@ -10,44 +10,52 @@ is connected to.
 Note that pos is not passed as a parameter.
 
 
+Set data structure can store not only int or string but also node.
 
-Result:
+Result: 56 ms
 """
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+
+# Method to find a loop in the linked list
+def is_cycle(node):
+    # Store each node to set
+    s = set()
+    while node:
+        # If current node is visited before, return true
+        if node is s:
+            return True
+        # If current node is first visited, add to set
+        s.add(node)
+        node = node.next
+
+    return False
+
+
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        if  head is None or head.next is None:
+        if head is None or head.next is None:
             return False
 
-        values = []
-        while True:
-            # Return false if there is no more node : acyclic
-            if head.next is None:
-                return False
-            else:
-                values.append(head.val)
-                head = head.next    # Update to next node
-                
-            for i in range(len(values)):
-                # # Current element appears in the list more than once
-                # if values.count(values[i]) > 1:
-                    for j in range(i + 1, len(values)):
-                        if values[i] == values[j] and values[j-1] == values[2*j-i-1]:
-                            return True
+        if is_cycle(head):
+            return True
+        else:
+            return False
 
-
-
-
-
-
-
-
-
-
-
-
+    # LeetCode example code, 48 ms
+    # This code changes node's value to mark visited nodes
+    def hasCycle(self, head: ListNode) -> bool:
+        while head:
+            # Max val is 100000
+            if head.val > 100000:
+                return True
+            # Update the value of visited node to 100001
+            head.val = 100001
+            head = head.next
+        return False
