e2bfd0a8149ec67136831d2802f397720029790d
wonkwonlee <dload1392@gmail.com>
2021-04-03 01:25:11 +0900

    Update 0005_longestPalindrome.py
    Need modifications.
    Not done yet.

--- Problems/0005_longestPalindrome.py
+++ Problems/0005_longestPalindrome.py
@@ -5,7 +5,27 @@ Given a string s, return the longest palindromic substring in s.
 Palindrome is a word which reads the same backward as forward, such as madam.
 
 Result: 
+
+실패 케이스 : "cbbd"
 """
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        
+        check = False
+        start = 0
+        left, right = [], []
+        
+        for i in range(len(s)):
+            left.append(s[i])
+            right.append(s[-1 - i])
+            
+            if check and left[i] == right[i]:
+                return "".join(left[start:i+1] + right[-i:-start])
+            
+            if left[i] == right[i]:
+                check = True
+                start = i
+                print(start)
