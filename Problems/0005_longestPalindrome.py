e2d898c89d1d9ef5eb2c6d7ded574dc117f1516a
wonkwonlee <dload1392@gmail.com>
2021-04-03 23:26:27 +0900

    Update 0005_longestPalindrome.py
    Solve longestPalindrome LeetCode problem 5.
    Refer to web source.
    Need review.

--- Problems/0005_longestPalindrome.py
+++ Problems/0005_longestPalindrome.py
@@ -1,9 +1,7 @@
-    """
+"""
 Longest Palindromic Substring
 Given a string s, return the longest palindromic substring in s.
-
 Palindrome is a word which reads the same backward as forward, such as madam.
-
 
 # String -> List
 str.split() : Split string by space
@@ -16,33 +14,37 @@ new_str = "".join(list) :
 # Reverse a list
 list.reverse() : Reverse the original list
 reversed(arr) : Return reversed list
-    list(reversed(arr))
-    for i in reversed(arr): 
-        print(i)
+list(reversed(arr))
+for i in reversed(arr):
+    print(i)
+
+# Reverse a string
+str_reverse = str[:: -1]
+
+Dynamic Programing
 
 
-Result: 
-
-실패 케이스 : "cbbd"
+Result: 104 ms
 """
+
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
+        if len(s) <= 1:
             return s
-        
-        list_s = list(s)
-        # print(s)
-        # print(list_s)
-        
-        for i in range(len(list_s)):
-            left, right = list_s[i], list_s[-i-1]
-            if left == right:
-                result = list_s[i:-i]
-                reverse_result = list(reversed(result))
-                print(result)
-                print(reverse_result)
-#                 print("Same")
-                
-#                 if reverse_result == result
-#                     return "".join(result)
+
+        ptr1, ptr2 = 0, 0
+        # Loop through string s with index i
+        for i in range(len(s)):
+            # Check substring is same as revered substring
+            if s[i - ptr2: i + 1] == s[i - ptr2: i + 1][::-1]:
+                # Move pointers to current substring
+                # ptr1 : ptr1 is i - previous ptr2
+                # ptr2 : As i increases by 1, ptr2 increases by 1
+                ptr1, ptr2 = i - ptr2, ptr2 + 1
+                # print(s[ptr1: ptr1+ptr2])
+            elif i - ptr2 > 0 and s[i - ptr2 - 1: i + 1] == s[i - ptr2 - 1: i + 1][::-1]:
+                ptr1, ptr2 = i - ptr2 - 1, ptr2 + 2
+                # print(s[ptr1: ptr1+ptr2])
+
+        return s[ptr1: ptr1 + ptr2]
