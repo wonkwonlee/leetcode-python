23fabc8bb4e1626c0ae162ff58d8f0f091d132a0
wonkwonlee <dload1392@gmail.com>
2021-04-08 21:05:41 +0900

    Create removeElements.py
    Create LeetCode Linked List problem, removeElements.'

---
+++ Linked-List/removeElements.py
@@ -0,0 +1,29 @@
+"""
+Remove Linked List Elements
+Given the head of a linked list and an integer val, remove all the nodes of the 
+linked list that has Node.val == val, and return the new head.
+
+
+Result:
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+
+        curr = head
+        while curr:
+            if curr.val == val:
+                break
+            prev = curr
+            curr = curr.next  
+
+        prev.next = curr.next
+        curr = None
+        
+        return 
