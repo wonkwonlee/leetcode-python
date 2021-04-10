4d76032e52c2e5aaf60e71ef113e6900a5d85092
wonkwonlee <dload1392@gmail.com>
2021-04-10 19:17:12 +0900

    Finish 0009_isPalindrome.py
    Solve LeetCode problem 9, isPalindrome.py.

--- Problems/0009_isPalindrome.py
+++ Problems/0009_isPalindrome.py
@@ -5,11 +5,16 @@ Given an integer x, return true if x is palindrome integer.
 An integer is a palindrome when it reads the same backward as forward. 
 For example, 121 is palindrome while 123 is not.
 
-
-Result:
+Result: 44 ms
 """
 
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
+        if x < 0:
+            return False
+            
+        # Integer to string
+        x = str(x)
+        if x == x[-1::-1]:
+            return True
