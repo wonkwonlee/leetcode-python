194ce844535f854cee8068f9f0e890a4e0af52c9
wonkwonlee <dload1392@gmail.com>
2021-04-10 19:00:00 +0900

    Finish removeElements.py
    Finish LeetCode Linked List problem removeElements.py.

--- Linked-List/removeElements.py
+++ Linked-List/removeElements.py
@@ -5,10 +5,13 @@ linked list that has Node.val == val, and return the new head.
 
 
 # Shallow Copy vs Deep Copy
+Initialize a new pointer node to shallow copy the original node.
+Move pointer node to track and return the original node.
+Any modification in the pointer node affects the original.
 
-
-Result:
+Result: 64 ms
 """
+
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -18,15 +21,22 @@ Result:
 
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
+        # Remove first node if value is val
+        while head is not None and head.val == val:
+            head = head.next
 
+        # Return None if head is None
+        if head is None:
+            return head
+
+        # Shallow copy of head node
         curr = head
-        while curr:
-            if curr.val == val:
-                break
-            prev = curr
-            curr = curr.next  
+        # Loop if curr and next node are both not None and stops if next is None
+        while curr is not None and curr.next is not None:
+            # If next value is val, continue to the node after next node
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
 
-        prev.next = curr.next
-        curr = None
-        
-        return 
+        return head
