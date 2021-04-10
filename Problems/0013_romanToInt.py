8f88fbbda5ebeefef0bccef3656c67fc86ab9de7
wonkwonlee <dload1392@gmail.com>
2021-04-10 22:46:40 +0900

    Format update

--- Problems/0013_romanToInt.py
+++ Problems/0013_romanToInt.py
@@ -64,8 +64,7 @@ class Solution:
         return value
 
 
-# Example using dictionary
-class Solution:
+    # Example using dictionary
     def romanToInt(self, s: str) -> int:
         roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5,
                  'I': 1}
