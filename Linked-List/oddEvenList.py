5a20e3b4f3ced60b7166be8a15d0434174643f6d
wonkwonlee <dload1392@gmail.com>
2021-04-11 20:22:43 +0900

    Create oddEvenList.py

---
+++ Linked-List/oddEvenList.py
@@ -0,0 +1,20 @@
+"""
+Odd Even Linked List
+Given the head of a singly linked list, group all the nodes with odd indices
+together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+Note that the relative order inside both the even and odd groups should remain.
+
+Result:
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        
