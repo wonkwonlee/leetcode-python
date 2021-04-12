fa9976590cf870e437cb72f334f39b7b2cefe670
wonkwonlee <dload1392@gmail.com>
2021-04-12 22:02:22 +0900

    Solve maxArea.py
    Solve LeetCode problem 11 Container with Most Water.

--- Problems/0011_maxArea.py
+++ Problems/0011_maxArea.py
@@ -12,32 +12,58 @@ Output: 49
 Input: height = [4,3,2,1,4]
 Output: 16
 
-# Greedy Algorithm
+# Sorted with lambda
+a = [(1, 2), (0, 1), (5, 1), (5, 2), (3, 0)]
+ 
+b = sorted(a)
+>> b = [(0, 1), (1, 2), (3, 0), (5, 1), (5, 2)]
 
-Result:
+c = sorted(a, key = lambda x : x[0])
+>> c = [(0, 1), (1, 2), (3, 0), (5, 1), (5, 2)]
+
+d = sorted(a, key = lambda x : x[1])
+>> d = [(3, 0), (0, 1), (5, 1), (1, 2), (5, 2)]
+
+e = sorted(e, key = lambda x : (x[0], -x[1]))
+>> e = [(0, 1), (1, 2), (3, 0), (5, 2), (5, 1)]
+
+Result: 724 ms
 """
 
 
 class Solution:
+    # Brute Force - Time Limit Exceeded
     def maxArea(self, height: List[int]) -> int:
         xy_plane = []
-
         for i in range(len(height)):
-            # (heightm width) = [(1, 1), (8, 2), (6, 3), (2, 4), (5, 5), (4, 6), (8, 7), (3, 8), (7, 9)]
-            xy_plane.append((height[i], i + 1))
-        
-        
-        # [(8, 7), (8, 2), (7, 9), (6, 3), (5, 5), (4, 6), (3, 8), (2, 4), (1, 1)]
-        xy_plane.sort(reverse=True)
-        print(xy_plane)
-        
-        result, area = 0, 0
-        for i in range(1, len(xy_plane)):
-            if xy_plane[i - 1][0] == xy_plane[i] == 0:
-                h
-            else:
-                h = min(xy_plane[i - 1][0], xy_plane[i][0])
-                w = abs(xy_plane[i - 1][1] - xy_plane[i][1])
+            xy_plane.append((height[i], i + 1))  # xy_plane = [(height, width)]
+
+        # Descending order by the first and ascending by the second element
+        xy_plane.sort(key=lambda x: (-x[0], x[1]))
+        result = 0
+        for i in range(len(xy_plane)):
+            for j in range(i + 1, len(xy_plane)):
+                h = min(xy_plane[i][0], xy_plane[j][0])
+                w = abs(xy_plane[i][1] - xy_plane[j][1])
                 result = max(result, h * w)
-        
+
+        return result
+
+    # Two Pointer Approach
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        result = 0
+        while left <= right:
+            h = min(height[left], height[right])
+            w = right - left
+            result = max(result, h * w)
+
+            # The right is shorter than the left
+            if height[left] >= height[right]:
+                right -= 1
+            # The left is shorter than the right 
+            else:
+                left += 1
+
         return result
