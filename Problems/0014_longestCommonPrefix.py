0699f6b8b816802121cbdb52a97a4a77eaf86c74
wonkwonlee <dload1392@gmail.com>
2021-04-12 22:02:43 +0900

    Create 0014_longestCommonPrefix.py
    Create LeetCode problem 14.

---
+++ Problems/0014_longestCommonPrefix.py
@@ -0,0 +1,19 @@
+"""
+14. Longest Common Prefix
+Find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+# Example
+Input: ["flower","flow","flight"]
+Output: "fl"
+
+Input: ["dog","racecar","car"]
+Ouput: ""
+
+Result:
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
