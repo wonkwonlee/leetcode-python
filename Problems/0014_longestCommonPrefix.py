8acf3e6c292bd0ed2a415848dc7aff14f48df5d7
wonkwonlee <dload1392@gmail.com>
2021-04-13 23:02:00 +0900

    Update 0014_longestCommonPrefix.py

--- Problems/0014_longestCommonPrefix.py
+++ Problems/0014_longestCommonPrefix.py
@@ -16,4 +16,41 @@ Result:
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
         
+        if len(strs) == 1:
+            return strs[0]
+
+        strs.sort(key=lambda x: len(x))
+
+        # Compare the first two strings
+        common = ""
+        index = 0
+        for char in strs[0]:
+            if char == strs[1][index]:
+                common = common + char
+                index += 1
+            else:
+                break
+
+        count = 0
+        for i in range(2, len(strs)):
+            if common != strs[i][0:len(common)]:
+                if common == "":
+                    return ""
+                common = common[0:-1]
+
+        return common
+
+
+
+
+
+
+
+
+
+
+
+
