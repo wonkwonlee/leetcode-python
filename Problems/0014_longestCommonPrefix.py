35c7bb92ec10b13723c1ae506e58aad0b1a369e8
wonkwonlee <dload1392@gmail.com>
2021-04-14 16:55:24 +0900

    Solve 0014_longestCommonPrefix.py
    Solve LeetCode problem 14 Longest Common Prefix.

--- Problems/0014_longestCommonPrefix.py
+++ Problems/0014_longestCommonPrefix.py
@@ -8,9 +8,9 @@ Input: ["flower","flow","flight"]
 Output: "fl"
 
 Input: ["dog","racecar","car"]
-Ouput: ""
+Output: ""
 
-Result:
+Result: 20 ms
 """
 
 
@@ -18,39 +18,32 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs) == 0:
             return ""
-        
+
         if len(strs) == 1:
             return strs[0]
 
+        # Sort strings by length
         strs.sort(key=lambda x: len(x))
 
         # Compare the first two strings
         common = ""
         index = 0
         for char in strs[0]:
+            # Store char if the char is in the both strings
             if char == strs[1][index]:
-                common = common + char
+                common += char
                 index += 1
             else:
                 break
 
-        count = 0
+        # Compare current common prefix with other string in the list
         for i in range(2, len(strs)):
-            if common != strs[i][0:len(common)]:
+            # Only compare the length of common prefix
+            while common != strs[i][0:len(common)]:
+                # If current common prefix is empty, return it
                 if common == "":
                     return ""
+                # Reduce common prefix from the end
                 common = common[0:-1]
 
         return common
-
-
-
-
-
-
-
-
-
-
-
-
