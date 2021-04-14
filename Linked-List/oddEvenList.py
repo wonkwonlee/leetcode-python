474be6642cca07f595c1180e4ee778565abf8f59
wonkwonlee <dload1392@gmail.com>
2021-04-14 16:54:16 +0900

    Format update

--- Linked-List/oddEvenList.py
+++ Linked-List/oddEvenList.py
@@ -15,6 +15,7 @@ Result:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
         count = 1
