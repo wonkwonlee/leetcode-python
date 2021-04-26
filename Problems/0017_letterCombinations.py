e356e1f89803408bafd0da5a1ffd71a4b5226099
wonkwonlee <dload1392@gmail.com>
2021-04-26 22:37:52 +0900

    Update 0017_letterCombinations.py
    Update LeetCode problem 17 Letter Combination.
    Not done yet.

--- Problems/0017_letterCombinations.py
+++ Problems/0017_letterCombinations.py
@@ -18,19 +18,29 @@ Result:
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        from itertools import product
+
         dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
                 '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
         result = []
-        temp = []
+        nums = []
 
-        if digits == "":
+        if len(digits) == 0:
             return result
 
-        for num in range(len(digits)):
-            temp.append(dic.get(digits[num]))     # abc, def
-
         for i in range(len(digits)):
-            temp[i]
+            nums.append(dic.get(digits[i]))
 
+        print(nums)
+        
+        for i in range(len(nums)):
+            for j in product(nums[i]):
+                result.append(j)
+                
         return result
+
+
+
+
+
