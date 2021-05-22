3b611afec243c9a4dfb955d742c32923a07bbae1
wonkwonlee <dload1392@gmail.com>
2021-05-22 18:05:42 +0900

    Update 0018_fourSum.py
    Not done yet.

--- Problems/0018_fourSum.py
+++ Problems/0018_fourSum.py
@@ -8,12 +8,25 @@ Given an array nums of n integers, return an array of all the unique quadruplets
 
 You may return the answer in any order.
 
-
 Result:
 """
 
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        # Length is less than 4
+        if len(nums) < 4:
+            return result
+        # Length is 4
+        if len(nums) == 4:
+            if sum(nums) == target:
+                result.append([nums[0], nums[1], nums[2], nums[3]])
+            return result
 
-        return None
+        nums.sort()
+
+        # -2 -1 0 0 1 2
+        for i in range(len(nums) - 2):
+            
+
