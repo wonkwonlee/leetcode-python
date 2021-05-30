f127848e09cf2a824557ac52fbf5ced82967fa28
wonkwonlee <28593767+wonkwonlee@users.noreply.github.com>
2021-05-30 20:18:46 +0900

    Solve 0020_isValid.py
    Solve LeetCode problem 20, Valid Parentheses.

--- Problems/0020_isValid.py
+++ Problems/0020_isValid.py
@@ -8,13 +8,18 @@ An input string is valid if:
 1. Open brackets must be closed by the same type of brackets.
 2. Open brackets must be closed in the correct order.
 
-Result:
+Result: 20 ms
 """
 
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        pair = {'(': ')', '{': '}', '[': ']'}
+        # Open and close brackets
+        open_bracket = ['(', '{', '[']
+        close_bracket = [')', '}', ']']
+
+        # Dictionary to store open and close bracket pair
+        pair = dict(zip(open_bracket, close_bracket))
 
         if len(s) % 2 != 0:
             return False
@@ -22,14 +27,27 @@ class Solution:
         if s[0] in ')}]':
             return False
 
-        # Initialize a stack
-        # First In, Last Out (FILO)
-        in_stack = []
-        in_stack.append(s[0])
+        # Initialize a stack (First In, Last Out)
+        stack = []
 
-        # ()[]{}
-        for i in range(1, len(s)):
-            if s[i] == pair[s[i - 1]]:
-                in_stack.pop()
+        for i in range(len(s)):
+            # If char is in open bracket
+            if s[i] in open_bracket:
+                # Push char to stack
+                stack.append(pair[s[i]])
+            # If char is in close bracket
             else:
-                in_stack.append(s[i])
+                # If stack is not empty and char is same as top
+                if stack and s[i] == stack[-1]:
+                    # Remove top from the stack
+                    stack.pop()
+                # Else return false
+                else:
+                    return False
+
+        # If stack is empty
+        if not stack:
+            return True
+        # If stack is not empty
+        else:
+            return False
